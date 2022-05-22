266d80a4bab60b4a3dc5ea75eedeb5b5e1841dfd
Danilo Nicacio de Lucena <70544466+DaniloNicacio@users.noreply.github.com>
2022-05-22 12:31:19 -0400

    Dobro, triplo e raiz quadrada
    Utilizando a biblioteca math o código calcula o dobro, triplo e a raiz quadrada de um número real qualquer inserido na função op_arit.

---
+++ EX006.py
@@ -0,0 +1,11 @@
+import math
+def op_arit(a):
+    dobro = a * 2
+    triplo = a * 3
+    raiz = math.sqrt(a)
+    print(f"O dobro de {a} vale: {dobro}")
+    print(f"O triplo de {a} vale: {triplo}")
+    print(f"A raiz de {a} vale: {raiz}")
+
+op_arit(9)
+
