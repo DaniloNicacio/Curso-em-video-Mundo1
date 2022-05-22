e8d46e23af81f67faebbcc2de1e4b60f67cc1b4d
Danilo Nicacio de Lucena <70544466+DaniloNicacio@users.noreply.github.com>
2022-05-22 12:28:49 -0400

    Verificar se a variável é de um determinado tipo
    O código verifica se a variável a:
    •Está em maiúsculo;
    •É um número;
    •É uma letra do alfabeto;
    •É um caractere ASCII;
    •É um número decimal;
    •É um digito;
    •É possível imprimir.

---
+++ EX004.py
@@ -0,0 +1,10 @@
+a = input("Digite algo: ")
+print(type(a))
+print(a.isupper())
+print(a.isalnum())
+print(a.isalpha())
+print(a.isascii())
+print(a.isdecimal())
+print(a.isdigit())
+print(a.isprintable())
+
