4a1b4a808a25ca0520afe8efa7c645d3ad7c7aec
Danilo Nicacio de Lucena <70544466+DaniloNicacio@users.noreply.github.com>
2022-05-22 12:49:06 -0400

    Seno, cosseno e tangente

---
+++ EX018.py
@@ -0,0 +1,8 @@
+from math import sin, cos, tan, radians
+
+def calcular_s_c_t(angulo):
+    a = radians(angulo)
+    print(f"O Seno deste angulo vale: {sin(a)}\n O Cosseno deste angulo vale: {cos(a)}\n A tangente deste angulo vale : {tan(a)}\n")
+
+calcular_s_c_t(30)
+
