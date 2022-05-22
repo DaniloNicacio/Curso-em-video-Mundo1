38f3078e42732ced2f245a326977ee30a6665cca
Danilo Nicacio de Lucena <70544466+DaniloNicacio@users.noreply.github.com>
2022-05-22 13:10:01 -0400

    Maior e menor
    O código verifica quem são os maiores e menores números inseridos

---
+++ EX033.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um número qualquer: \n"))
+n2 = int(input("Digite um número qualquer: \n"))
+n3 = int(input("Digite um número qualquer: \n"))
+
+print(max(n1,n2,n3))
+print(min(n1,n2,n3))
